6cc62f6fb240508c29a03a898516ffffb2bd36a2
sakib01773 <sakibshohan1@gmail.com>
2022-11-09 05:28:21 +0600

    Connent with MongoDb and setup env variable

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express')
 const cors = require('cors')
+require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
+
+
+
 
 //midleWare
 app.use(cors())
 app.use(express.json())
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aebmoeb.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 
 app.get('/', (req, res) => {
     res.send('Hello From Tourist WebSite>>>')
