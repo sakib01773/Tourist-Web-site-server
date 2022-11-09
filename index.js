3dd90a34e6dbd2ca4f824bfc496354d749893255
sakib01773 <sakibshohan1@gmail.com>
2022-11-09 06:48:29 +0600

    Limit data load from data base

--- index.js
+++ index.js
@@ -17,8 +17,41 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aebmoeb.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// async function run() {
+//     try {
+//         const userCollection = client.db("tourist-app").collection("services")
+
+//         app.get("/services", async (req, res) => {
+//             const quary = {}
+//             const cursor = userCollection.find(quary);
+//             const services = await cursor.toArray();
+//             res.send(services);
+//         })
+        
+//     }
+//     finally {
+        
+//     }
+// }
+// run().catch(err => console.log(err))
 
 
+async function run() {
+    try {
+        const serviceCollection = client.db("tourist-app").collection("services");
+        app.get("/homeservices", async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query).limit(3);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+                    
+    }
+    finally {
+                    
+    }
+}
+run().catch(err => console.log(err))
 app.get('/', (req, res) => {
     res.send('Hello From Tourist WebSite>>>')
   })
